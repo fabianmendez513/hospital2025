400cdc38c12fc3931aa099a8e0e63a6e83c1bdbe
fabianmendez513 <fabianisaimendez@gmail.com>
2025-05-09 15:52:40 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main (String [] args) {
+        //Sumar dos numeros con un metodo
+    Programas.sumar(1,5);
+    Programas.resta(10,5);
+    }
+}
---
+++ Programas.java
@@ -0,0 +1,11 @@
+public class Programas {
+    
+    public static void sumar( int a , int b){
+        int resultado = a + b;
+        System.out.println("Resultado" + resultado);
+    }
+
+    public static int resta (int a , int b ){
+        return a-b;
+    }
+}
